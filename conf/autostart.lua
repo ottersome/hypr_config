c3863104f7e12c43b5d14fee4d5a75b3ee2347e9
ottersome <luisgarcia26284@gmail.com>
2026-06-20 10:37:31 -0600

    adding: autostart for dbus and keyring

--- conf/autostart.lua
+++ conf/autostart.lua
@@ -8,6 +8,8 @@ hl.on("hyprland.start", function ()
   hl.exec_cmd("wl-paste --type text --watch cliphist store") -- Stores only text data
   hl.exec_cmd("wl-paste --type image --watch cliphist store") -- Stores only image data
   hl.exec_cmd("tmux setenv -g HYPRLAND_INSTANCE_SIGNATURE \"$HYPRLAND_INSTANCE_SIGNATURE\"")
-  hl.exec_cmd("exec-once=xrdb ~/.Xresources # Purely for sxiv colors")
-  hl.exec_cmd("exec-once = systemctl --user start hyprpolkitagent")
+  hl.exec_cmd("xrdb ~/.Xresources # Purely for sxiv colors")
+  hl.exec_cmd("systemctl --user start hyprpolkitagent")
+  hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+  hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
 end)
