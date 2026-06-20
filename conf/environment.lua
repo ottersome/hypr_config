bac85af3ee7abf1d2decf94c43f9fd1123c791d4
ottersome <luisgarcia26284@gmail.com>
2026-06-20 10:18:16 -0600

    adding: bat pager

--- conf/environment.lua
+++ conf/environment.lua
@@ -9,3 +9,6 @@ hl.env("QT_QPA_PLATFORMTHEME","qt6ct")
 hl.env("QT_QPA_PLATFORMTHEME","qt5ct")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION","1")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+
+-- Utils
+hl.env("PAGER", "bat")
